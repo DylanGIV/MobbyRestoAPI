3383a6658a8e6aebeb122cd481a92266a445dc52
Dylan Gonzalez <dylangiv123@gmail.com>
2021-05-06 01:59:52 -0700

    add user retrieval

---
+++ Migrations/20210506083155_userSelect.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MobbyRestoAPI.Migrations
+{
+    public partial class userSelect : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
